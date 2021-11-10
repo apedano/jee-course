45367c438ee0f4685792d8059ffac31770b7704e
Alessandro Pedano <pedano.alessandro@gmail.com>
2021-11-10 18:34:33 +0100

    Producers and disposers

--- 002-todo-webapp/src/main/java/com/apedano/jeecourse/todo/producers/LoggerProducer.java
+++ 002-todo-webapp/src/main/java/com/apedano/jeecourse/todo/producers/LoggerProducer.java
@@ -1,15 +1,38 @@
 package com.apedano.jeecourse.todo.producers;
 
+import com.apedano.jeecourse.todo.annotations.CarQualifier;
+import com.apedano.jeecourse.todo.entity.Car;
+import com.apedano.jeecourse.todo.entity.Vehicle;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.enterprise.inject.Disposes;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
+@Slf4j
 public class LoggerProducer {
 
     @Produces
     public Logger prouduceLogger(InjectionPoint injectionPoint) {
         return LoggerFactory.getLogger("[PRODUCED] " + injectionPoint.getMember().getDeclaringClass().getName());
+    }
+
+    @Produces
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Produces
+    @CarQualifier
+    private Vehicle produceVehicle() {
+        return new Car();
+    }
+
+    private void doDispose(@Disposes Vehicle vehicle) {
+        log.info("Disposing contextual instance:{}", vehicle);
     }
 }
